7c98c218306708dcaf77b5cd346b6271d79936ae
monkeymango55 <90772792+monkeymango55@users.noreply.github.com>
2022-06-15 00:11:20 -0500

    Initial Commit

---
+++ Estee Lauder Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Estee Lauder Challenge
+//
+//  Created by Sophia on 6/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
